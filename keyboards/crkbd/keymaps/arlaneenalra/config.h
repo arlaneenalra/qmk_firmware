a57f29d00e79500c4f0d5c21966539d4b224a5c1
arlaneenalra <emeraldd.chris@gmail.com>
2023-05-16 22:33:00 -0500

    Add autoshift to the keymap and get rid of the shifted Z layer .. that didn't work ...

--- keyboards/crkbd/keymaps/arlaneenalra/config.h
+++ keyboards/crkbd/keymaps/arlaneenalra/config.h
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 #define EE_HANDS
 #define SPLIT_WPM_ENABLE
+#define AUTO_SHIFT_REPEAT
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
