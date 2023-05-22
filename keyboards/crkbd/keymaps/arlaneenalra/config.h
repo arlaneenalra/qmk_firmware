953c4f7123af536448584443ea74a8672684c37b
arlaneenalra <emeraldd.chris@gmail.com>
2023-05-21 20:00:54 -0500

    add one-shot-toggle.

--- keyboards/crkbd/keymaps/arlaneenalra/config.h
+++ keyboards/crkbd/keymaps/arlaneenalra/config.h
@@ -36,6 +36,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_SHIFT_TIMEOUT 150
 
 #define TAPPING_TOGGLE 2
+#define ONESHOT_TAP_TOGGLE 2
 
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
