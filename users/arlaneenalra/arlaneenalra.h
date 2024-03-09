085095b10ddf2714440d14ea97ca866bf857bf9d
arlaneenalra <emeraldd.chris@gmail.com>
2024-03-09 17:10:31 -0600

    Add new way to get to number layer.

--- users/arlaneenalra/arlaneenalra.h
+++ users/arlaneenalra/arlaneenalra.h
@@ -76,7 +76,7 @@ enum layer_names {
       QK_GESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC, \
       CW_TOGG,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,                             KC_H,  HOME_J,  HOME_K,  HOME_L,HME_SCLN, KC_QUOT, \
        KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                             KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT, \
-                                           KC_INS,   LOWER,        __VA_ARGS__,        RAISE,  KC_DEL )
+                              LT(_NUMBER, KC_INS),   LOWER,        __VA_ARGS__,        RAISE,  KC_DEL )
 
 #define AE_LOWER_LAYER(_LAYOUT_, ...) _LAYOUT_( \
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______, \
